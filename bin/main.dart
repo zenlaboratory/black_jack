905fea6bd89e1ff4b53497a63d56b510699e9c9c
Omar Alvarez <zenlaboratory@gmail.com>
2020-04-29 12:06:28 +0200

    3rd commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,5 @@
-import 'dart:io';
 import 'package:black_jack/black_jack.dart' as black_jack;
-import 'package:test/test.dart';
 
 void main(List<String> arguments) {
-  var minimoAleatorio = 0;
-  var maximoAleatorio = 0;
-
-  stdout.write('Introduce el valor mínimo del aleatorio: ');
-  minimoAleatorio = int.parse(stdin.readLineSync());
-  stdout.write('Introduce el valor máximo del aleatorio: ');
-  maximoAleatorio = int.parse(stdin.readLineSync());
-  do {
-    print(black_jack.NumeroAleatorio(minimoAleatorio, maximoAleatorio));
-  } while (minimoAleatorio != 0);
+  black_jack.MezclarBaraja();
 }
--- lib/black_jack.dart
+++ lib/black_jack.dart
@@ -1,9 +1,22 @@
 import 'dart:math';
 
-int NumeroAleatorio(int min, max) {
-  var random = Random();
-  var numeroAleatorio = 0;
+var random = Random();
+var numeroAleatorio = 0;
+var barajaValores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+var barajaPalos = ['C', 'D', 'P', 'T'];
+var barajaMezclada = <Map>{};
 
+int NumeroAleatorio(int min, max) {
   numeroAleatorio = (min + random.nextInt((max + 1) - min));
   return numeroAleatorio;
+}
+
+void MezclarBaraja() {
+  while (barajaMezclada.length < 52) {
+    barajaMezclada.add({
+      barajaValores[NumeroAleatorio(0, 12)]: barajaPalos[NumeroAleatorio(0, 3)]
+    });
+  }
+  print(barajaMezclada.length);
+  print(barajaMezclada);
 }
